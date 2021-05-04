32fe1703ff2448b796e7d5cc5c037c5bf1323631
Hasin Hayder <countdraculla@gmail.com>
2021-05-04 17:17:14 +0600

    Vue3 + Vite + WindiCSS (Tailwind) Boilerplate

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1 class="text-green-500 text-4xl my-5">{{ msg }}</h1>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button class="my-5 py-2 px-4 font-semibold rounded-lg shadow-md bg-green-500 hover:bg-green-700 text-white" @click="state.count++">count is: {{ state.count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import WindiCSS from 'vite-plugin-windicss'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    WindiCSS()
+  ],
+  server: {
+    open: '/index.html',
+  }
+})
